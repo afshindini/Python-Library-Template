806c37fc166e650db115bdc2744080857b34c9c5
Afshin Dini <afshin_dini@yahoo.com>
2024-11-26 00:13:05 +0200

    Modify logging to be more percise

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/logging.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/logging.py
@@ -1,20 +1,30 @@
 """Logger initialization"""
 
 import logging
+import logging.config
 from typing import Any, Optional
 
 
 def config_logger(loglevel: int, log_file: Optional[str] = None) -> Any:
     """Initialize a custom logger"""
-    logger = logging.getLogger()
-    logger.setLevel(loglevel)
-    formatter = logging.Formatter(
-        "{asctime} - {levelname} - {message}", style="{", datefmt="%Y-%m-%d %H:%M:%S"
-    )
-    console_handler = logging.StreamHandler()
-    logger.addHandler(console_handler)
-    console_handler.setFormatter(formatter)
-    if log_file:
-        file_handler = logging.FileHandler(log_file, mode="a", encoding="utf-8")
-        logger.addHandler(file_handler)
-        file_handler.setFormatter(formatter)
+    default_logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            'standard': {
+                'format': '%(asctime)s - [%(levelname)s] [%(name)s.%(funcName)s:%(lineno)d (%(process)d)] | %(message)s',
+                'datefmt': '%Y-%m-%d %H:%M:%S',
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "standard",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": loglevel,
+        },
+    }
+    logging.config.dictConfig(default_logging_config)
